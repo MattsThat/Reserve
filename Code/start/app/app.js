fc07a4fb843b1a40c0c7e14f41bf1db5ad05329f
KimiR <vikramvm@yahoo.com>
2018-08-20 23:25:15 +0900

    layoutv0.01

--- Code/start/app/app.js
+++ Code/start/app/app.js
@@ -1,63 +1,10 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
+import { render } from 'react-dom';
+import Demo from './gridlayout';
 
-var App = React.createClass({
-    render:function(){
-       return(
-        <div>
-        <Grid fluid="true">
-            <Row className="mytop">
-                <Col className="mytop">
-                  <code>hello</code>
-                </Col>
-                  <Col className="mytop">
-                  <code>helllooooo</code>
-                </Col>
-            </Row>
-            <Row className="mytop">
-                  <Col><code>cs</code></Col>
-            </Row>
-            {/*
-            <Row className="show-grid">
-                <Col xs={6} md={4}>
-                <code>&lt;{'Col xs={6} md={4}'} /">">&gt;</code>
-                <br/>
-                Vikram
-                </Col>
-                <Col xs={6} md={4}>
-                <code>&lt;{'Col xs={6} md={4}'} /">">&gt;</code>
-                <br/>
-                Vikram
-                </Col>
-                <Col xsHidden md={4}>
-                <code>&lt;{'Col xsHidden md={4}'} /">">&gt;</code>
-                <br/>
-                Vikram
-                </Col>
-            </Row>
-
-            <Row className="show-grid">
-                <Col xs={6} xsOffset={6}>
-                <code>&lt;{'Col xs={6} xsOffset={6}'} /">">&gt;</code>
-                </Col>
-            </Row>
-
-            <Row className="show-grid">
-                <Col md={6} mdPush={6}>
-                <code>&lt;{'Col md={6} mdPush={6}'} /">">&gt;</code>
-                </Col>
-                <Col md={6} mdPull={6}>
-                <code>&lt;{'Col md={6} mdPull={6}'} /">">&gt;</code>
-                </Col>
-            </Row>*/}
-            </Grid>
-      </div>
-          )
-    }
-});
-
-ReactDOM.render(<App/>,document.getElementById("main"));
+const rootElement = document.querySelector('#root');
+if (rootElement) {
+  render(<Demo/>, rootElement);
+}
+    
